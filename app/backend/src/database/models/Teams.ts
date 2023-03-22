57ef36d6680540ff87edad58835c44131e8cbc6a
Matheus Augusto Fonseca <matheus7asf@gmail.com>
2023-03-22 18:56:48 -0300

    migration e models de teams

---
+++ app/backend/src/database/models/Teams.ts
@@ -0,0 +1,29 @@
+import { INTEGER, Model, STRING } from 'sequelize';
+import db from '.';
+// import OtherModel from './OtherModel';
+
+class Teams extends Model {
+  // declare <campo>: <tipo>;
+  declare id: number;
+  declare teamName: string;
+}
+
+Teams.init({
+  id: {
+    type: INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  teamName: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+});
+
+export default Teams;
